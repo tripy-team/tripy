f53d04a1f3b2c187670d524f326a4e266928b2d7
Eric <ezhong0211@gmail.com>
2025-08-11 19:31:30 -0700

    feat(Backend infrastructure): Query best flights from a city's nearby airport to the other city's nearby airport
    Generate a list in dollars of the amount it costs to go from one airport to the other

--- backend/python_code/calculationLambda.py
+++ backend/python_code/calculationLambda.py
@@ -1,20 +1,25 @@
 import json
-from backend.python_code.classes import User, City, Node
+from backend.python_code.classes import User, Airport, Trip, City, Flight
 import networkx as nx
+from itertools import permutations
+import serpapi
 
 
 def lambda_handler(index, context):
     pass
 
 
-def generate_itinerary(users, cities: list[City]):
+def generate_shared_itinerary(users: list[User], cities: list[Airport]):
     """Itineraries (for now) only include flights and hotels"""
-    city_graph = create_graph(cities)
-    for city in cities:
+    for user in users:
+        start_airport = user.start_loc
+        end_airport = user.end_loc
+        user.airport_graph = create_user_graph(cities)
+    for airport in cities:
         pass
 
 
-def optimize(users: list[User], cities: list[City] = None):
+def optimize(users: list[User], cities: list[Airport] = None):
     airline_prefs = set()
     hotel_prefs = set()
     for user in users:
@@ -22,13 +27,13 @@ def optimize(users: list[User], cities: list[City] = None):
         hotel_prefs.add(user.hotel_prefs)
 
 
-def get_flights_from_city(city: City):
+def get_flights_from_airport(airport: Airport):
     """returns the outbound flights in the format
-    "city_code" :[
+    "airport_code" :[
         {
         "airport_code": Airport
         "flight": Flight
-        "end_city": City,
+        "end_airport": airport,
         "cost_in_dollars": int,
         "cost_in_miles": int
         }
@@ -37,23 +42,75 @@ def get_flights_from_city(city: City):
     pass
 
 
-def create_graph(cities: list[City]):
-    """we are assuming that everyone is starting from the same city, initially"""
+def create_user_trip(user: User):
+    start_loc = user.start_loc
+    end_loc = user.end_loc
+
+
+def create_user_graph(
+    start_airport: Airport, end_airport: Airport, destinations: list[Airport]
+):
+    """we are assuming that everyone is starting from the same airport, initially"""
     G = nx.digraph()
     all_flights = {}
-    for city in cities:
-        start_city_code = city.city_code
-        flights_from_city = get_flights_from_city(city)
-        all_flights[start_city_code] = flights_from_city
-        G.add_node(start_city_code)
-        for flight in flights_from_city[start_city_code]:
-            end_city_code = flight["end_city"].city_code
+    for destination in destinations:
+        start_airport_code = destination.airport_code
+        flights_from_airport = get_flights_from_airport(destination)
+        all_flights[start_airport_code] = flights_from_airport
+        G.add_node(start_airport_code)
+        for flight in flights_from_airport[start_airport_code]:
+            end_airport_code = flight["end_airport"].airport_code
             cost_in_dollars = flight["cost_in_dollars"]
             cost_in_miles = flight["cost_in_miles"]
             flight_duration = flight["flight"].duration
-            G.add_node(end_city_code)
-            G.add_edge(start_city_code, end_city_code, weight=flight_duration)
+            G.add_node(end_airport_code)
+            G.add_edge(start_airport_code, end_airport_code, weight=flight_duration)
     return G
+
+
+def create_itinerary(start_city: City, end_city: City, destinations: list[City]):
+    destination_permutations = permutations(destinations)
+    for permutation in destination_permutations:
+        pass
+
+
+def get_cheapest_path_between_cities(user, start_city, end_city):
+    pass
+
+
+def get_airports_from_city(city: City) -> list[Airport]:
+    pass
+
+
+# for suggestions, keep calling solo_trip for destinations and cities
+# look for a way to determine how many days is good enough to travel to one place
+# should be able to suggest multiple countries or one country with multiple destinations
+# might be good to filter at the end of all the calculations and give suggestions if nothing matches
+
+
+def create_solo_trip(
+    user: User, start_date, end_date, desired_cities: list[City], start_city, end_city
+):
+    best_option = (0, float("inf"))  # dollars, points
+    start_airports = get_airports_from_city(start_city)
+    end_airports = get_airports_from_city(end_date)
+    node_index = 0
+    trip_graph = nx.digraph()
+    trip_graph[node_index] = (start_city, 0)
+    for start_airport in start_airports:
+        running_date = start_date
+        for desired_city in desired_cities:
+            flights_from_start_airport_to_specific_city = get_flights_from_airport(
+                start_airport, desired_city, running_date
+            )
+            for flight in flights_from_start_airport_to_specific_city:
+                pass
+
+
+def get_flights_from_airport_to_specific_city(
+    airport, city, start_date
+) -> list[Flight]:
+    pass
 
 
 if __name__ == "__main__":
--- backend/python_code/managerLambda.py
+++ backend/python_code/managerLambda.py
@@ -4,26 +4,16 @@ from dotenv import load_dotenv
 import os
 import boto3
 from amadeus import Client, Location, ResponseError
+import allcities
+from serpapi import GoogleSearch
+from classes import Flight, Airport
 
 
 def lambda_handler(index, context):
     pass
 
 
-def test():
-    auth_url = "https://test.api.amadeus.com/v1/security/oauth2/token"
-    auth_payload = {
-        "grant_type": "client_credentials",
-        "client_id": os.getenv("AMADEUS_API_KEY"),
-        "client_secret": os.getenv("AMADEUS_API_SECRET"),
-    }
-
-    auth_response = requests.post(auth_url, data=auth_payload)
-    access_token = auth_response.json()
-    print(access_token)
-
-
-def tester():
+def get_airport_data_from_city(city, countryCode):
     load_dotenv()
     amadeus = Client(
         client_id=os.getenv("AMADEUS_API_KEY"),
@@ -31,21 +21,48 @@ def tester():
     )
 
     response = amadeus.reference_data.locations.get(
-        keyword="LON", subType=Location.AIRPORT
+        countryCode=countryCode, keyword=city, subType=Location.AIRPORT
     )
-    print(response.result)
+    city_airport_data_list = response.result["data"]
+    return city_airport_data_list
 
 
-def lambda_handler(index, context):
-    lambda_client = boto3.client("lambda")
-    pass
+def get_flights_between_cities(
+    start_city, start_city_countryCode, end_city, end_city_countryCode, filters=None
+):
+    start_airport_data = get_airport_data_from_city(start_city, start_city_countryCode)
+    end_airport_data = get_airport_data_from_city(end_city, end_city_countryCode)
+    flights_price_dict = {}
+
+    for start_airport in start_airport_data:
+        start_airport_iataCode = start_airport["iataCode"]
+        for end_airport in end_airport_data:
+            end_airport_iataCode = end_airport["iataCode"]
+            params = {
+                "deep_search": True,
+                "engine": "google_flights",
+                "departure_id": start_airport_iataCode,
+                "arrival_id": end_airport_iataCode,
+                "outbound_date": "2025-08-18",
+                "api_key": os.getenv("SERPAPI_KEY"),
+                "type": 2,
+                "currency": "USD",
+            }
+            search = GoogleSearch(params)
+            results = search.get_dict()
+            try:
+                for best_flight in results["best_flights"]:
+                    print(best_flight["price"])
+            except:
+                continue
+    print(flights_price_dict)
 
 
-def send_rewards_links_to_webscraping_lambda(client, links):
-    for link in links:
-        response = client.invoke(FunctionName="")
+def get_flights_cost_in_points():
     pass
 
 
 if __name__ == "__main__":
-    tester()
+    load_dotenv()
+    get_flights_between_cities("Seattle", "US", "Orlando", "US")
+    # get_airport_data_from_city("Seattle", "US")
