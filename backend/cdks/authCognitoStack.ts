6f84a9b488fffe72a7a484f57116089e5299c58e
Eric <ezhong0211@gmail.com>
2025-08-20 18:47:55 -0700

    feat(User authentication): Create cdk for cognito partially
    Remove unecessary apiGateway and built partial cdk for auth

---
+++ backend/cdks/authCognitoStack.ts
@@ -0,0 +1,73 @@
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Stack, StackProps } from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+
+export interface authCognitoStackProps extends StackProps {}
+
+export class authCognitoStack extends Stack {
+  constructor(scope: Construct, id: string, props: authCognitoStackProps) {
+    super(scope, id, props);
+
+    const requiredAndMutable = {
+      required: true,
+      mutable: true,
+    };
+    const userpool = new cognito.UserPool(this, `${id}-userpool`, {
+      userPoolName: "tripy-userpool",
+      signInCaseSensitive: true,
+
+      //sign ups
+      selfSignUpEnabled: true,
+      userVerification: {
+        emailSubject: "Welcome to Tripy!",
+        emailBody:
+          "Thank you for signing up for Tripy! Your verification code is {####}",
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+        smsMessage: "Welcome to Tripy! Your verification code is {####}",
+      },
+      standardAttributes: {
+        fullname: requiredAndMutable,
+        email: requiredAndMutable,
+        phoneNumber: requiredAndMutable,
+        birthdate: requiredAndMutable, //need to include above 18 functionality,
+        gender: requiredAndMutable, //drop down for male, female, non-binary, prefer not to answer
+      },
+
+      //sign ins
+      signInAliases: {
+        email: true,
+        phone: true,
+      },
+      autoVerify: {
+        email: true,
+        phone: true,
+      },
+      signInPolicy: {
+        allowedFirstAuthFactors: {
+          emailOtp: true,
+          smsOtp: true,
+          password: false,
+        },
+      },
+
+      //account recovery
+      accountRecovery: cognito.AccountRecovery.EMAIL_AND_PHONE_WITHOUT_MFA,
+
+      //account security https://docs.aws.amazon.com/cognito/latest/developerguide/cognito-user-pool-settings-threat-protection.html
+
+
+      //device tracking
+      deviceTracking:{
+        challengeRequiredOnNewDevice: true,
+        deviceOnlyRememberedOnUserPrompt: true
+      },
+      
+      //read about lambda triggers section
+    });
+    const lambdaTriggerRole = new iam.Role(this, "lambdaTrigger", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+    userpool.grant(lambdaTriggerRole, "cognito-idp:AdminCreateUser");
+  }
+}
